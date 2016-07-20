d90363f4d52efd03ed8f4120e82013b462ee6796
craigambrose <craig@craigambrose.com>
2016-07-20 21:24:18 +0200

    an indexed row of day buttons

--- src/elm/PersonRow.elm
+++ src/elm/PersonRow.elm
@@ -13,8 +13,12 @@ import DayButton
 -- MODEL
 
 type alias Model = {
-  mon: DayButton.Model,
-  tue: DayButton.Model
+  days: List IndexedDayButton
+}
+
+type alias IndexedDayButton = {
+  id : Int,
+  model : DayButton.Model
 }
 
 init : (Model, Cmd Msg)
@@ -22,33 +26,55 @@ init =
     let
       startDate = dateFromFields 2016 Jan 1 0 0 0 0
     in
-      ({
-        mon = DayButton.init startDate,
-        tue = DayButton.init (Date.Extra.Period.add Date.Extra.Period.Day 1 startDate)
-      }, Cmd.none)
+      (
+        {
+          days = [
+            initIndexedDayButton startDate 0,
+            initIndexedDayButton startDate 1,
+            initIndexedDayButton startDate 2,
+            initIndexedDayButton startDate 3,
+            initIndexedDayButton startDate 4,
+            initIndexedDayButton startDate 5,
+            initIndexedDayButton startDate 6
+          ]
+        },
+        Cmd.none
+      )
+
+initIndexedDayButton startDate index =
+  IndexedDayButton index (DayButton.init (Date.Extra.Period.add Date.Extra.Period.Day index startDate))
 
 -- UPDATE
 
 type Msg =
-  Mon DayButton.Msg |
-  Tue DayButton.Msg
+  Modify Int DayButton.Msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
   case action of
-    Mon msg ->
-      ({ model | mon = DayButton.update msg model.mon }, Cmd.none)
-    Tue msg ->
-      ({ model | tue = DayButton.update msg model.tue }, Cmd.none)
+    Modify id msg ->
+      ({ model | days = List.map (updateHelp id msg) model.days }, Cmd.none)
+
+updateHelp : Int -> DayButton.Msg -> IndexedDayButton -> IndexedDayButton
+updateHelp targetId msg {id, model} =
+  IndexedDayButton id (if targetId == id then DayButton.update msg model else model)
+
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
+  let
+    dayButtons = List.map viewIndexedDayButton model.days
+  in
     div [] [
       span [] [ text "Craig A." ],
-      div [ class "btn-group" ] [
-        App.map Mon (DayButton.view model.mon),
-        App.map Tue (DayButton.view model.tue)
-      ]
+      div [ class "btn-group" ] dayButtons
     ]
+        -- App.map Mon (DayButton.view model.mon),
+        -- App.map Tue (DayButton.view model.tue)
+
+
+viewIndexedDayButton : IndexedDayButton -> Html Msg
+viewIndexedDayButton {id, model} =
+  App.map (Modify id) (DayButton.view model)
